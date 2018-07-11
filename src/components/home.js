ccba5416550940e5e1b8a1c5eaad6917675ac22a
StephanieLDR <stephanie.c.lacina@gmail.com>
2018-07-11 15:36:24 -0700

    added nav menu and routing to diff pages

---
+++ src/components/home.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default props => {
+    return (
+        <div>
+            <h1 className="text-center">Welcome to Movie Quotes</h1>
+            <p><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Dolorum eius nostrum odit, quod sapiente sed temporibus ut velit.
+                Ab amet aspernatur explicabo incidunt laudantium maxime officiis praesentium quia,
+                saepe voluptate.</span>
+                <span>Cupiditate dolor error, exercitationem id ipsam laboriosam
+                maiores mollitia pariatur praesentium quaerat quisquam quos. Asperiores consequatur necessitatibus nobis provident quisquam.
+                Distinctio dolore earum maiores minima molestias, odio odit pariatur sint.</span></p>
+        </div>
+    )
+}
